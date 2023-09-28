017c950f2270ba94605e113abc6c89cad95580e8
Chandika Herath <chandikasbh@gmail.com>
2023-09-28 19:36:06 +1300

    Updated with edit and delete code

--- September2023/Pages/TMPage.cs
+++ September2023/Pages/TMPage.cs
@@ -55,14 +55,81 @@ namespace September2023.Pages
             }
         }
 
-        public void EditTimeRecord(IWebDriver driver) 
-        { 
+        public void EditTimeRecord(IWebDriver driver)
+        {
             //Code for Edit Time Record
+            IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+            goToLastPageButton.Click();
+            Thread.Sleep(3000);
+
+            //Click on Edit Button
+            IWebElement editButton = driver.FindElement(By.XPath("//tbody/tr[last()]/td[5]/a[1]"));
+            editButton.Click();
+            Thread.Sleep(3000);
+
+            //Edit Code in Code Textbox
+            IWebElement editCodeTextbox = driver.FindElement(By.Id("Code"));
+            editCodeTextbox.Clear();
+            editCodeTextbox.SendKeys("IC2023Edited");
+
+            //Edit Description in Description Textbox
+            IWebElement editDescriptionTextBox = driver.FindElement(By.Id("Description"));
+            editDescriptionTextBox.Clear();
+            editDescriptionTextBox.SendKeys("IC2023Edited");
+
+            //Edit Price in Price Textbox
+            IWebElement editPriceOverlappingTag = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+            IWebElement editPriceTextBox = driver.FindElement(By.Id("Price"));
+            editPriceOverlappingTag.Click();
+            editPriceTextBox.Clear();
+            editPriceOverlappingTag.Click();
+            editPriceTextBox.SendKeys("500");
+
+            //Click on save button
+            IWebElement editSaveButton = driver.FindElement(By.Id("SaveButton"));
+            editSaveButton.Click();
+            Thread.Sleep(7000);
+
+            // Clock on goToLastPage Button
+            IWebElement editGoToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+            editGoToLastPageButton.Click();
+
+            IWebElement editedCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+            IWebElement EditedDescription = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[3]"));
+
+            if (editedCode.Text == "IC2023Edited" && EditedDescription.Text == "IC2023Edited")
+            {
+                Console.WriteLine("Time Record has been updated successfully");
+            }
+            else
+            {
+                Console.WriteLine("Time Record has not been updated");
+            }
         }
 
-        public void DeleteTimeRecord(IWebDriver driver) 
+        public void DeleteTimeRecord(IWebDriver driver)
         {
             //Code for Delete Time Record
+            IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+            goToLastPageButton.Click();
+            Thread.Sleep(3000);
+
+            //Click on delete button
+            IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
+            deleteButton.Click();
+
+            IAlert simpleAlert = driver.SwitchTo().Alert();
+            simpleAlert.Accept();
+
+            IWebElement lastCodeInTable = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+            if (lastCodeInTable.Text == "IC2023Edited")
+            {
+                Console.WriteLine("Time Record has not been deleted");
+            }
+            else
+            {
+                Console.WriteLine("Time Record has been deleted successfully");
+            }
         }
     }
 }
